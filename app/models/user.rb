572bcc0ea10eb846ef28c8a3bb5751d4c05597c6
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-31 12:45:33 +0600

    Update password using activeModel securePassword

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+ include ActiveModel::SecurePassword
+  has_secure_password
+
+  attr_accessor :password_digest
+
   validates :user_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :avatar, allow_nil: true
   validates :blocked, inclusion: { in: [true, false] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_192811) do
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "avatar"
     t.integer "role"
     t.boolean "blocked"
