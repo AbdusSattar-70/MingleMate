6f334467176ba44bf2970eb7a80347e2b4e65d9f
Abdus Sattar <sasorganicagro@gmail.com>
2024-01-02 04:10:44 +0600

    Refactor devise authentication

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,27 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params
   include RackSessionFix
   respond_to :json
 
-  # POST /signup
-  def create
-      @user = User.new(user_params)
-
-      if @user.save
-        render json: {message: 'User Created Successfully!'}, status: :ok
-      else
-        render json: { message: "Unable to Create: #{@user.errors.full_messages}"}, status: :unprocessable_entity
-      end
-  end
-
   private
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :email, :password])
-  end
-
-  def user_params
-    params.require(:user).permit(:user_name, :email, :password)
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {message: 'Signed up successfully.'}, status: :ok
+    else
+      render json: {message: "Registration failed. #{current_user.errors.full_messages.to_sentence}"}, status: :unprocessable_entity
+    end
   end
 
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
   include RackSessionFix
-  before_action :configure_sign_in_params, only: [:create]
   respond_to :json
 
   def create
     @user = authenticate_user
 
     if @user
-      sign_in_user
-      render_successful_sign_in
+        sign_in(:user, @user)
+        render json: { message: 'Successfully Signed In', data: serialized_user_attributes(@user) }
     else
       render_unauthorized
     end
@@ -17,50 +16,33 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def respond_to_on_destroy
-    handle_user_sign_out
-  rescue JWT::DecodeError
+    decode_jwt
+  if current_user
+    sign_out(current_user)
+    render json: { message: 'User Signed Out Successfully!' }, status: :ok
+  else
     render_unauthorized
   end
-
-  def configure_sign_in_params
-    params.require(:user).permit(:email, :password)
   end
 
   def authenticate_user
     User.find_for_database_authentication(email: sign_in_params[:email])
   end
 
-  def sign_in_user
-    sign_in(:user, @user)
-  end
-
-  def render_successful_sign_in
-    jwt_token = JWT.encode({ sub: @user.id }, Rails.application.credentials.fetch(:secret_key_base))
-    render json: { message: 'Successfully Signed In', data: serialized_user_attributes(@user) }
-  end
-
   def render_unauthorized
     render json: { message: 'Unauthorized' }, status: :unauthorized
   end
 
-  def handle_user_sign_out
-    jwt_token = request.headers['Authorization']&.split(' ')&.last
-
-    if jwt_token
-      jwt_payload = JWT.decode(jwt_token, Rails.application.credentials.fetch(:secret_key_base)).first
-      user_id = jwt_payload['sub']
-      current_user = User.find_by(id: user_id)
-
-      if current_user
-        sign_out(current_user)
-        render json: { message: 'User Signed Out Successfully!' }, status: :ok
-      else
-        render_unauthorized
-      end
-    else
-      render_unauthorized
+ def decode_jwt
+  if request.headers['Authorization'].present?
+    begin
+      jwt_payload = JWT.decode(request.headers['Authorization']&.split(' ')&.last, Rails.application.credentials.devise_jwt_secret_key!)&.first
+      current_user = User.find(jwt_payload['sub'])
+    rescue StandardError
+      return
     end
   end
+end
 
   def serialized_user_attributes(resource)
     UserSerializer.new(resource).serializable_hash[:data][:attributes]
