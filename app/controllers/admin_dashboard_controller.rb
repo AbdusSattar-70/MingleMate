dc8123d3c32047f97d715a516e469c28d11f830b
Abdus Sattar <sasorganicagro@gmail.com>
2024-01-02 23:39:38 +0600

    Add admin_dashboard controller to handle admin's activities

---
+++ app/controllers/admin_dashboard_controller.rb
@@ -0,0 +1,66 @@
+class AdminDashboardController < ApplicationController
+  before_action :set_users, only: [:index]
+  before_action :authenticate_user!
+
+  # GET /users
+  def index
+    render json: @users
+  end
+
+ def block_multiple
+    handle_multiple_action(true, 'Blocked succeeded', 'Failed to block')
+  end
+
+  def unblock_multiple
+    handle_multiple_action(false, 'Unblocked succeeded', 'Failed to unblock')
+  end
+
+  def destroy_multiple
+    handle_multiple_action(nil, 'Delete succeeded', 'Failed to delete')
+  end
+
+def toggle_role
+  user_emails = params[:user_emails]
+  users = User.where(email: user_emails)
+
+  users.each do |user|
+    user.update(role: toggle_role_value(user.role))
+  end
+
+  render json: { message: 'Role toggled successfully' }, status: :ok
+end
+
+  private
+
+  def set_users
+    @users = User.all
+
+    # Filter by role
+    @users = @users.where(role: params[:role]) if params[:role].present?
+
+    # Filter by status
+    @users = @users.where(blocked: params[:blocked]) if params[:blocked].present?
+    # Default ordering if no parameter is provided
+    @users = @users.order(:id)
+  end
+
+  def handle_multiple_action(blocked, success_message, failure_message)
+    user_emails = params[:user_emails]
+    users = User.where(email: user_emails)
+
+    if blocked.nil? ? users.destroy_all : users.update_all(blocked: blocked)
+      render json: { message: success_message }, status: :ok
+    else
+      render json: { message: failure_message }, status: :unprocessable_entity
+    end
+  end
+
+  def toggle_role_value(current_role)
+    current_role == 1 ? 2 : 1
+  end
+
+
+  def user_params
+    params.require(:user).permit(:email, :role, :blocked, user_emails: [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   defaults format: :json do
+
+  resources :users,  only: [ :show, :update, :destroy]
+  resources :items
+  resources :categories
+  resources :tags
+  resources :likes
+  resources :comments
+  resources :collections
+
   devise_for :users, path: '', path_names: {
     sign_in: 'signin',
     sign_out: 'signout',
@@ -10,14 +19,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+# routes for only admin user
   get '/current_user', to: 'current_user#index'
-  resources :users,  only: [:index, :show, :update, :destroy]
-  resources :items
-  resources :categories
-  resources :tags
-  resources :likes
-  resources :comments
-  resources :collections
+  get 'admin/users', to: 'admin_dashboard#index'
+  patch 'admin/users/block', to: 'admin_dashboard#block_multiple'
+  patch 'admin/users/unblock', to: 'admin_dashboard#unblock_multiple'
+  patch 'admin/users/role_toggle', to: 'admin_dashboard#toggle_role'
+  delete 'admin/users/delete', to: 'admin_dashboard#destroy_multiple'
   get "up" => "rails/health#show", as: :rails_health_check
   end
 end
