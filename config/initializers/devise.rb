323387673ef4b099f271eee1fd3663b32c9744af
Abdus Sattar <a90.sattar@gmail.com>
2025-07-23 15:46:55 +0600

    Regenerate rails secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -42,7 +42,7 @@ Devise.setup do |config|
   config.navigational_formats = []
 
   config.jwt do |jwt|
-  jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+  jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
   jwt.dispatch_requests = [
     ['POST', %r{^/sign_in$}]
   ]
